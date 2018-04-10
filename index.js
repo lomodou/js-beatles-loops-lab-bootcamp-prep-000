100d6525bcdd134605cbadf8f002b72a54f1d29f
Modou Lo <modoulo99@icloud.com>
2018-04-10 05:55:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+function theBeatlesPlay(musicians, instruments) {
+  
+  var phrases = [];
+  
+  for (var i = 0; i < musicians.length; i++) {
+    
+    phrases.push(musicians[i] + " plays " + instruments[i])
+    
+  }
+  return phrases
+}
